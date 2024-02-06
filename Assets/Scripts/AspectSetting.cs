271d4ae4e9cc3565740f677c1a6253d6dc4176e0
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-02-06 14:42:57 +0900

    [update]アスペクト比の設定

---
+++ Assets/Scripts/AspectSetting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AspectSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(540, 960, FullScreenMode.Windowed);
+    }
+}
